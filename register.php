e7c92afb28aed2800a111f6ada35567a72049fb3
risnahamin <125646058+risnahamin@users.noreply.github.com>
2024-06-17 13:48:32 +0900

    Add files via upload

--- register.php
+++ register.php
@@ -1,62 +1,63 @@
 <?php 
-include 'header.php';
-?>
+include '../koneksi/koneksi.php';
+$kode = mysqli_query($conn, "SELECT kode_customer from customer order by kode_customer desc");
+$data = mysqli_fetch_assoc($kode);
+$num = substr($data['kode_customer'], 1, 4);
+$add = (int) $num + 1;
+if(strlen($add) == 1){
+	$format = "C000".$add;
+}else if(strlen($add) == 2){
+	$format = "C00".$add;
+}
+else if(strlen($add) == 3){
+	$format = "C0".$add;
+}else{
+	$format = "C".$add;
+}
 
-<div class="container" style="padding-bottom: 250px;">
-	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Register</b></h2>
-	<form action="proses/register.php" method="POST">
-		<div class="row">
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="exampleInputPassword1">Nama</label>
-					<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Nama" name="nama" required>
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="exampleInputPassword1">Email</label>
-					<input type="email" class="form-control" id="exampleInputPassword1" placeholder="Email" name="email" required>
-				</div>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="exampleInputPassword1">username</label>
-					<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Username" name="username" required >
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="exampleInputPassword1">No Tepl</label>
-					<input type="text" class="form-control" id="exampleInputPassword1" placeholder="+62" name="telp" required >
-				</div>
-			</div>
-
-		</div>
+$nama = $_POST['nama'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$tlp = $_POST['telp'];
+$konfirmasi = $_POST['konfirmasi'];
 
 
-		<div class="row">
-			
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="exampleInputPassword1">Password</label>
-					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password" required>
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="exampleInputPassword1">Konfirmasi Password</label>
-					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Konfirmasi Password" name="konfirmasi" required>
-				</div>
-			</div>
-		</div>
 
-		<button type="submit" class="btn btn-success">Register</button>
-	</form>
-</div>
+$hash = password_hash($password, PASSWORD_DEFAULT);
 
-<?php 
-include 'footer.php';
+if($password == $konfirmasi){
+	$cek = mysqli_query($conn, "SELECT username from customer where username = '$username'");;
+	$jml = mysqli_num_rows($cek);
+
+	if($jml == 1){
+		echo "
+		<script>
+		alert('USERNAME SUDAH DIGUNAKAN');
+		window.location = '../register.php';
+		</script>
+		";
+		die;
+	}
+
+	$result = mysqli_query($conn, "INSERT INTO customer VALUES('$format','$nama', '$email', '$username', '$hash', '$tlp')");
+	if($result){
+		echo "
+		<script>
+		alert('REGISTER BERHASIL');
+		window.location = '../user_login.php';
+		</script>
+		";
+	}
+
+}else{
+	echo "
+	<script>
+	alert('KONFIRMASI PASSWORD TIDAK SAMA');
+	window.location = '../register.php';
+	</script>
+	";
+}
+
+
 ?>
